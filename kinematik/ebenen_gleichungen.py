1f61720987d2741e23c1eea63a0cf489fedc4270
lweyand <lweyand@htw-saarland.de>
2026-02-20 18:28:39 +0100

    Added float type to ebenen_gleichung

--- kinematik/ebenen_gleichungen.py
+++ kinematik/ebenen_gleichungen.py
@@ -3,10 +3,10 @@ import numpy as np
 import math
 
 def func_z_in_ebene(position, n_vek, d):
-    
-    z_punkt = (-position[0] * n_vek[0] - position[1] * n_vek[1] - d) / n_vek[2]
-    float(z_punkt)
-    return z_punkt
+
+    z_punkt = (position[0] * n_vek[0] + position[1] * n_vek[1] - d) / n_vek[2]
+
+    return float(np.asarray(z_punkt).squeeze())
 
 def func_d_fur_ebene(n_vek, punkt):
     # 0 = n_vektor[0] * stutze_v_u_pos[0] + n_vektor[1] * stutze_v_u_pos[1] + n_vektor[2] * platte_hohe_std + d
