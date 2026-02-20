ad63d8178d03eb4ab0faa22c1e2e8c02f0c10342
Humbucker007-007 <seha00007@htwsaar.de>
2026-02-20 18:14:11 +0100

    float added

--- kinematik/ebenen_gleichungen.py
+++ kinematik/ebenen_gleichungen.py
@@ -5,7 +5,7 @@ import math
 def func_z_in_ebene(position, n_vek, d):
     
     z_punkt = (-position[0] * n_vek[0] - position[1] * n_vek[1] - d) / n_vek[2]
-
+    float(z_punkt)
     return z_punkt
 
 def func_d_fur_ebene(n_vek, punkt):
