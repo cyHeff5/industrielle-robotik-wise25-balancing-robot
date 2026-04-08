f4d72edb13ce23dd91ce8fffabdfdce1ea309392
lweyand <lweyand@htw-saarland.de>
2026-04-08 22:12:07 +0200

    Remove unused PDController (replaced by PIDController)

--- control/__init__.py
+++ control/__init__.py
@@ -1,4 +1,3 @@
-from .pd_controller import PDController
 from .pid_controller import LowPassFilter, PIDController
 
-__all__ = ["PDController", "PIDController", "LowPassFilter"]
+__all__ = ["PIDController", "LowPassFilter"]
